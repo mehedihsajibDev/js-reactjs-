97f9c3050e8901255ae29c25c5e2b9232d666baf
sajib <mhsajib44@gmail.com>
2025-01-13 19:23:45 +0600

    filter

---
+++ mstep/src/Title.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function (props) {
+  return (
+    <div>
+     <h2>Your New resul is {props.number}</h2>
+     <div>
+     <h2>Your New resul is {props.nickname}</h2>
+    </div>
+    </div>
+    
+  )
+}
